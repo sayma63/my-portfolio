9cf1128248c95ae48758a98a6e2f3cc1ca173f6c
sayma63 <saymasumi106353@gmail.com>
2022-06-10 09:06:23 +0600

    projects added

--- src/Pages/Home/Projects.js
+++ src/Pages/Home/Projects.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../Loading';
+import SingleProject from './SingleProject';
 
 const Projects = () => {
+    const [projects,setProjects]=useState([]);
+    const[loading,setLoading]=useState(true);
+    useEffect(()=>{
+        fetch('projects.json')
+        .then(res=>res.json())
+        .then(data=>{
+            setProjects(data)
+            setLoading(false);
+        })
+    },[])
     return (
         <div>
-            
+            {
+                 
+                    loading?<div><Loading></Loading></div>:null
+                
+            }
+            <h1 className='text-center text-purple-600 font-bold text-4xl mt-5 mb-6'>My Projects</h1>
+            <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-10 px-10 mb-10'>
+                {
+                    projects.map(project=><SingleProject key={project.id}
+                    project={project}></SingleProject>)
+                }
+
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Home/SingleProject.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SingleProject = ({project}) => {
+    const {id,name,img,description}=project;
+    return (
+        <div>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+       <figure className= 'px-10 pt-10'><img src={img} className="w-full" style={{height:'200px'}} alt="" /></figure>
+<div className="card-body">
+<h2 className="card-title font-bold text-blue-700">{name}</h2>
+<p><span className='text-xl'> Description:  {description?.length < 80 ? description : description.slice(0, 70)+"..read more.."}</span></p>
+
+
+
+
+<div className="card-actions justify-center">
+  <button className="btn btn-primary w-full" >Details</button>
+</div>
+</div>
+</div>
+    </div>
+    );
+};
+
+export default SingleProject;
