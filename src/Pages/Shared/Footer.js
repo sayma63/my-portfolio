2203b4a041cadc0d61848f484db1de46a9f065db
sayma63 <saymasumi106353@gmail.com>
2022-06-11 08:41:15 +0600

    not found added

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='static bottom-0  p-5'>
+        <div className=' w-full 
+         static bottom-0  p-5'>
             <h1 className='text-xl text-center'>Copyright &copy; 2022 ,All Rights Reserved</h1>
         </div>
     );
